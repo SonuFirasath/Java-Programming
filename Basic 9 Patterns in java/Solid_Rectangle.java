7176d704b9305d1890173583300eaa1f7850c516
F G Firasath <114803488+SonuFirasath@users.noreply.github.com>
2025-02-17 23:00:43 +0530

    Add files via upload

---
+++ Basic 9 Patterns in java/Solid_Rectangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solid_Rectangle {
+
+    public static void main(String[] args){
+        int n = 4;
+        int m = 5;
+
+        for(int i = 0;i<=n;i++)
+        {
+            for(int j = 0;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
